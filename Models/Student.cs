0dd628fb003cd1950b39c1928fb1a0efb47d93bf
Cheng-Ying Li <chengying.li@dwave.cc>
2024-07-29 12:31:41 +0800

    feat: add student

---
+++ Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace BasicDataTemplateSample.Models;
+
+public class Student : Person
+{
+    /// <summary>
+    /// Gets or sets the grade of the student. You can  only set this property on init.
+    /// </summary>
+    public int Grade { get; init; }
+}
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -17,6 +17,22 @@ public class MainWindowViewModel : ViewModelBase
             Age = 55,
             Sex=Sex.Diverse,
             Subject = "My Favorite Subject"
+        },
+        new Student
+        {
+            FirstName = "Hello",
+            LastName = "World",
+            Age = 17,
+            Sex = Sex.Male,
+            Grade = 12
+        },
+        new Student
+        {
+            FirstName = "Hello",
+            LastName = "Kitty",
+            Age = 12,
+            Sex = Sex.Female,
+            Grade = 6
         }
     };
 }
